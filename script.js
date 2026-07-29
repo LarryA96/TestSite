246648246532d3be4069ce64b1e592804b3a87db
LarryA96 <156938571+LarryA96@users.noreply.github.com>
2026-07-29 12:27:05 -0400

    Fetch weather data and display in a paragraph

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Create a new paragraph element
+const paragraph = document.createElement("p");
 
+//Change its text to new york data
+fetch('https://api.open-meteo.com/v1/forecast?latitude=40.73&longitude=-73.93')
+  .then(response => response.json())
+  .then(data => {
+paragraph.textContent = JSON.stringify(data);
+  });
+
+// Add it to the end of the page
+document.body.appendChild(paragraph);
